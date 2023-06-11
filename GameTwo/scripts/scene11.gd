9c740104bcfd105c656fba1f51663fa6cdca7e93
sam-jia <sam_jia@sfu.ca>
2023-06-11 11:23:14 -0700

    scene 10,11 finished

--- GameTwo/scripts/scene11.gd
+++ GameTwo/scripts/scene11.gd
@@ -1,40 +1,35 @@
 extends Node2D
 
-@onready var red_button = $red_button
-@onready var pink_button = $pink_button
-@onready var red_jelly = $m_jelly_left
-@onready var pink_jelly = $m_jelly_right
+@onready var purple_button = $purple_button
+@onready var purple_jelly = $m_jelly_purple
+@onready var no_color_jelly = $m_jelly_uncolored
 @onready var back_button = $back_button
 @onready var hint_box = $hint_box
 @onready var hint_text = $hint_text
-var showRed = func():
-	red_jelly.visible = false
+var showPurple = func():
+	no_color_jelly.visible = false
+	purple_jelly.visible = true
 	$correct_sound.play()
 
-var showPink = func():
-	pink_jelly.visible = false
-	$correct_sound.play()
 
 var go_back = func():
-	SceneTransition.get_tree().change_scene_to_file("res://GameTwo/scenes/Scene9.tscn")
+	SceneTransition.get_tree().change_scene_to_file("res://GameTwo/scenes/scene10.tscn")
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hint_box.visible = false
 	hint_text.visible = false
-	red_button.pressed.connect(showRed)
-	pink_button.pressed.connect(showPink)
+	purple_button.pressed.connect(showPurple)
 	back_button.pressed.connect(go_back)
-	
 	pass # Replace with function body.
 
 
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if pink_jelly.visible == false && red_jelly.visible == false:
+	if no_color_jelly.visible == false && purple_jelly.visible == true:
 		await get_tree().create_timer(1.0).timeout
-		SceneTransition.get_tree().change_scene_to_file("res://GameTwo/scenes/scene11.tscn")
+		SceneTransition.get_tree().change_scene_to_file("res://GameTwo/scenes/scene12.tscn")
 	pass
 
 
@@ -49,8 +44,17 @@ func _on_orange_button_pressed():
 	pass # Replace with function body.
 
 
+func _on_red_button_pressed():
+	$wrong_sound.play()
+	pass # Replace with function body.
+
 
 func _on_green_button_pressed():
+	$wrong_sound.play()
+	pass # Replace with function body.
+
+
+func _on_pink_button_pressed():
 	$wrong_sound.play()
 	pass # Replace with function body.
 
@@ -61,10 +65,5 @@ func _on_yellow_button_pressed():
 
 
 func _on_blue_button_pressed():
-	$wrong_sound.play()
-	pass # Replace with function body.
-
-
-func _on_purple_button_pressed():
 	$wrong_sound.play()
 	pass # Replace with function body.
