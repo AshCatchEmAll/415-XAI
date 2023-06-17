ebd19582721f11332ee6217ee24f1168242c7836
sam-jia <sam_jia@sfu.ca>
2023-06-16 21:54:14 -0700

    fixed scene5 transition

--- GameOne/scripts/Scene5.gd
+++ GameOne/scripts/Scene5.gd
@@ -49,7 +49,7 @@ func _process(delta):
 
 func _on_button_pressed():
 	
-	get_tree().change_scene_to_file("res://GameOne/scenes/DragDropScene.tscn")
+	SceneTransition.change_scene_to_file("res://GameOne/scenes/DragDropScene.tscn")
 
 
 
