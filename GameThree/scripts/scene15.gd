9830debaa2944782159282045c3a4ce2599b2d95
sam-jia <sam_jia@sfu.ca>
2023-07-01 11:52:04 -0700

    changed barnacles to only sequential access

--- GameThree/scripts/scene15.gd
+++ GameThree/scripts/scene15.gd
@@ -4,7 +4,10 @@ extends Node2D
 @onready var question2 = $question_text2
 @onready var question3 = $question_text3
 @onready var question4 = $question_text4
-
+@onready var q1solved = false
+@onready var q2solved = false
+@onready var q3solved = false
+@onready var q4solved = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$shovelAnimation.play("ShellAnimation")
@@ -22,32 +25,38 @@ func _on_help_button_pressed():
 
 
 func _on_barnacle_1_pressed():
-	question1.visible = true
-	question2.visible = false
-	question3.visible = false
-	question4.visible = false
+	if q1solved == true:
+		pass
+	else:
+		question1.visible = true
+		question2.visible = false
+		question3.visible = false
+		question4.visible = false
 	pass # Replace with function body.
 
 
 func _on_barnacle_2_pressed():
-	question1.visible = false
-	question2.visible = true
-	question3.visible = false
-	question4.visible = false
+	if q1solved == true:
+		question1.visible = false
+		question2.visible = true
+		question3.visible = false
+		question4.visible = false
 	pass # Replace with function body.
 
 
 func _on_barnacle_3_pressed():
-	question1.visible = false
-	question2.visible = false
-	question3.visible = true
-	question4.visible = false
+	if q2solved == true:
+		question1.visible = false
+		question2.visible = false
+		question3.visible = true
+		question4.visible = false
 	pass # Replace with function body.
 
 
 func _on_barnacle_4_pressed():
-	question1.visible = false
-	question2.visible = false
-	question3.visible = false
-	question4.visible = true
+	if q3solved == true:
+		question1.visible = false
+		question2.visible = false
+		question3.visible = false
+		question4.visible = true
 	pass # Replace with function body.
